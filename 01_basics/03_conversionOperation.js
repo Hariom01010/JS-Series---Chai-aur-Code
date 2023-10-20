ae8112fa209056acfb7df0a3f7b4a6c06910046b
Hariom Vashista <rishivashista4@gmail.com>
2023-10-20 23:57:50 +0000

    Add js file for conversion operation: Lecture 6

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN 
+
+let isLoggedIn = "hariom"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false 
+// "" => false 
+// "hariom" => true 
+
+
+let someNumber = 0
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
