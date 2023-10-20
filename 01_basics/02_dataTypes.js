7aaeccd0d561e41bd7e6c13e97871ff7010fab11
Hariom Vashista <rishivashista4@gmail.com>
2023-10-20 23:42:53 +0000

    Add js file for datatype for Lecture 5

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using NodeJS, not browser
+
+let name = "hariom"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// string => ""
+// bigint 
+// boolean  => true or false
+// null => standalone value
+// undefined => 
+// symbol 
+
+// object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
