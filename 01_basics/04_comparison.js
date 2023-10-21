f6368c52d62fad66a3d298620e2fbcc4046d27a7
Hariom Vashista <rishivashista4@gmail.com>
2023-10-21 00:47:31 +0000

    Add comparison js file to codebase for Lecture 8

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log( 2 > 1)
+// console.log( 2 >= 1)
+// console.log( 2 < 1)
+// console.log( 2 <= 1)
+// console.log( 2 == 1)
+// console.log( 2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+
+// console.log(null > 0)  //false
+// console.log(null == 0) //false
+// console.log(null >= 0) //true
+
+//Comparison operator (> < >= ...) converts null to 0 while equality (==) operator doesn't, that's why
+// null>0 is false and null>=0 is true.
+
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined > 0)
+
+// === : Doesn't convert data type
+console.log("2" === 2)
