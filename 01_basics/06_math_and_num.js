fdc100bc4ca955e191aa43298010c7c2fdca9dc2
Hariom Vashista <rishivashista4@gmail.com>
2023-10-21 14:02:53 +0000

    Add math, num, date js file for Lecture 12 and 13

---
+++ 01_basics/06_math_and_num.js
@@ -0,0 +1,35 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance)
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 1123.896
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ***************************** MATHS **********************************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,3,6,12,56))
+// console.log(Math.max(4,3,6,12,56))
+
+
+// console.log(Math.random())
+// console.log((Math.random() * 10) + 1)
+
+
+const min = 20
+const max = 30
+
+console.log(Math.floor((Math.random()*(max-min + 1)) + min))
---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,28 @@
+// Dates
+
+let mydate = new Date()
+
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString("en-IN"))
+// console.log(typeof mydate)
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
